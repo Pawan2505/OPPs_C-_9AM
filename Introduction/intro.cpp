f46fcce3a8d667c88e43bdf9c781e1bae4ca04c9
pawan2505 <pawanaktu2gmail.com>
2025-12-27 11:11:25 +0530

    vehical

--- Introduction/intro.cpp
+++ Introduction/intro.cpp
@@ -1793,7 +1793,7 @@
 //     // jo ki "silent garbage value" se behtar hai.
 
 
-// Polymorphism : 
+// Polymorphism : https://www.geeksforgeeks.org/cpp/cpp-polymorphism/
 
 // What is polymorphism in C++?
 
@@ -1891,3 +1891,218 @@
 // cout<<"Y : "<<a3.y<<endl;
 
 // }
+
+
+// Run - Time polymorphism 
+
+
+// #include<iostream>
+// using namespace std;
+
+// class Animal{
+//     public :
+
+//    virtual void speak(){
+//         cout<<"Every Animal Speack!"<<endl;
+//     }
+// };
+
+// class Dog: public Animal{
+// public :
+//   void speak()override{
+//         cout<<"Dog bark!"<<endl;
+//     }
+// };
+
+// int main(){
+
+//     Dog d1;
+
+//     d1.speak();
+
+// }
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicle {
+public:
+    int vehicleID;
+    string manufacturer;
+    string model;
+    int year;
+    static int totalVehicles;
+
+    Vehicle() {
+        totalVehicles++;
+    }
+
+    Vehicle(int id, string mfg, string mdl, int y) {
+        vehicleID = id;
+        manufacturer = mfg;
+        model = mdl;
+        year = y;
+        totalVehicles++;
+    }
+
+    ~Vehicle() {
+        totalVehicles--;
+    }
+
+  
+    int getVehicleID() { return vehicleID; }
+
+   void display() {
+        cout << "ID: " << vehicleID<< ", Manufacturer: " << manufacturer<< ", Model: " << model<< ", Year: " << year << endl;
+    }
+};
+
+int Vehicle::totalVehicles = 0;
+
+
+class Car : public Vehicle {
+public:
+    string fuelType;
+
+    Car(int id, string mfg, string mdl, int y, string fuel): Vehicle(id, mfg, mdl, y) {
+        fuelType = fuel;
+    }
+
+};
+
+class ElectricCar : public Car {
+public:
+    int batteryCapacity;
+
+    ElectricCar(int id, string mfg, string mdl, int y, string fuel, int battery): Car(id, mfg, mdl, y, fuel) {
+        batteryCapacity = battery;
+    }
+
+};
+
+class SportsCar : public ElectricCar {
+    public:
+    int topSpeed;
+
+    SportsCar(int id, string mfg, string mdl, int y, string fuel, int battery, int speed): ElectricCar(id, mfg, mdl, y, fuel, battery) {
+        topSpeed = speed;
+    }
+
+};
+
+
+class Sedan : public Car {
+public:
+    Sedan(int id, string mfg, string mdl, int y, string fuel): Car(id, mfg, mdl, y, fuel) {}
+};
+
+class SUV : public Car {
+public:
+    SUV(int id, string mfg, string mdl, int y, string fuel): Car(id, mfg, mdl, y, fuel) {}
+};
+
+
+class Aircraft {
+
+public:
+    int flightRange;
+
+    Aircraft(int range) {
+        flightRange = range;
+    }
+
+
+};
+
+class FlyingCar : public Car, public Aircraft {
+public:
+    FlyingCar(int id, string mfg, string mdl, int y, string fuel, int range): Car(id, mfg, mdl, y, fuel), Aircraft(range) {}
+
+};
+
+
+class VehicleRegistry {
+    Vehicle *vehicles[50];
+    int count;
+
+public:
+    VehicleRegistry() {
+        count = 0;
+    }
+
+    void addVehicle(Vehicle* v) {
+        vehicles[count++] = v;
+    }
+
+    void displayAll() {
+        for (int i = 0; i < count; i++) {
+            vehicles[i]->display();
+            cout << "------------------------" << endl;
+        }
+    }
+
+    void searchByID(int id) {
+        for (int i = 0; i < count; i++) {
+            if (vehicles[i]->getVehicleID() == id) {
+                vehicles[i]->display();
+                return;
+            }
+        }
+        cout << "Vehicle not found!" << endl;
+    }
+};
+
+int main() {
+    VehicleRegistry registry;
+    int choice;
+
+    do {
+        cout << "\nVehicle Registry Menu\n";
+        cout << "1. Add Sedan\n";
+        cout << "2. Add SUV\n";
+        cout << "3. Add Electric Sports Car\n";
+        cout << "4. View All Vehicles\n";
+        cout << "5. Search by ID\n";
+        cout << "0. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        int id, year, battery, speed;
+        string mfg, model, fuel;
+
+        switch (choice) {
+        case 1:
+            cout << "Enter ID, Manufacturer, Model, Year, Fuel: ";
+            cin >> id >> mfg >> model >> year >> fuel;
+            registry.addVehicle(new Sedan(id, mfg, model, year, fuel));
+            break;
+
+        case 2:
+            cout << "Enter ID, Manufacturer, Model, Year, Fuel: ";
+            cin >> id >> mfg >> model >> year >> fuel;
+            registry.addVehicle(new SUV(id, mfg, model, year, fuel));
+            break;
+
+        case 3:
+            cout << "Enter ID, Manufacturer, Model, Year, Fuel, Battery, Speed: ";
+            cin >> id >> mfg >> model >> year >> fuel >> battery >> speed;
+            registry.addVehicle(new SportsCar(id, mfg, model, year, fuel, battery, speed));
+            break;
+
+        case 4:
+            registry.displayAll();
+            break;
+
+        case 5:
+            cout << "Enter Vehicle ID: ";
+            cin >> id;
+            registry.searchByID(id);
+            break;
+        }
+    } while (choice != 0);
+
+    cout << "Total Vehicles: " << Vehicle::totalVehicles << endl;
+    return 0;
+}
